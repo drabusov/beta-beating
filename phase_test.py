d2005adc068c87e5c999be2a67880b694f9a78cf
drabusov <38492649+drabusov@users.noreply.github.com>
2019-01-18 18:28:44 +0100

    4 trajectories

--- phase_test.py
+++ phase_test.py
@@ -7,6 +7,7 @@ from orbit.teapot import teapot
 from orbit.teapot import TEAPOT_MATRIX_Lattice
 from orbit.bunch_generators import TwissContainer, GaussDist2D
 
+from orbit.orbit_correction import orbit
 
 from orbit.errors import AddErrorNode
 from orbit.errors import AddErrorSet
@@ -33,11 +34,9 @@ matrix_lattice = TEAPOT_MATRIX_Lattice(teapot_latt,b)
 
 #----------------------------------Bunch-Distribution------------------------------------------------
 # machted beam
-emittance_x, emittance_y = 10**(-5),10**(-5) # this number I get just to check the idea
+emittance_x, emittance_y = 35*10**(-6),15*10**(-6) # this number I get just to check the idea
 twissX = TwissContainer(arrPosAlphaX[0][1], arrPosBetaX[0][1], emittance_x)
 twissY = TwissContainer(arrPosAlphaY[0][1], arrPosBetaY[0][1], emittance_y)
-
-
 
 #----------------------------------------------------------------------------------------------------
 dist = GaussDist2D(twissX,twissY)
@@ -47,7 +46,7 @@ b.addParticle(x,px,y,py,0,0)
 
 
 
-turn = 1024
+turn = 512
 x_turn, px_turn = list(), list()
 
 for k in range(turn):
@@ -56,11 +55,10 @@ for k in range(turn):
     px_turn.append(b.px(0))
 
 plt.figure()
-plt.scatter(x_turn,px_turn,s=5)
+plt.scatter(x_turn,px_turn,s=5,color = 'blue', label = 'ideal orbit')
 plt.grid(True, ls = '--')
 plt.xlabel('x, m')
 plt.ylabel('$p_x$, rad')
-plt.title('One particle in during 1024 consequent turns. Ideal orbit')
 #plt.savefig('phase_space_x_ideal1.pdf')
 
 
@@ -77,32 +75,78 @@ paramsDict["subtype"]  = "TransDisp"
 paramsDict["sample"]      = "Uniform"
 paramsDict["maximum"]        = 0.5
 paramsDict["minimum"]       = 0.0
-paramsDict["dx"]       = 0.006
-paramsDict["dy"]       = 0.006
+paramsDict["dx"]       = 0.008
+paramsDict["dy"]       = 0.008
 
 setDict["elementtype"] = "quad"
 setDict["ringline"] = "ring"
 
 ESet  = AddErrorSet(teapot_latt, positioni, positionf, setDict, paramsDict, seed_value=50)
 #ESet  = AddErrorSet(lattice, positioni, positionf, setDict, paramsDict) # Random
+
+OrbitX, OrbitY = orbit(teapot_latt,b).get_orbit() 
+x0, px0 = OrbitX[0][1], OrbitX[0][2]
+y0, py0 = OrbitY[0][1], OrbitY[0][2]
 #---------------------------------------------ORBIT ERRORS-------------------------------------------
 
+b.deleteAllParticles()
+b.addParticle(x0+x,px0+px,y0+y,py0+py,0,0)
 
-
-x_turn, px_turn = list(), list()
+x_turn1, px_turn1, y_turn1, py_turn1 = list(), list(), list(), list()
 
 for k in range(turn):
     teapot_latt.trackBunch(b)
-    x_turn.append(b.x(0))
-    px_turn.append(b.px(0))
+    x_turn1.append(b.x(0))
+    px_turn1.append(b.px(0))
+    y_turn1.append(b.y(0))
+    py_turn1.append(b.py(0))
 
-plt.figure()
-plt.scatter(x_turn,px_turn,s=5)
+x00 = np.mean(x_turn1)
+px00 = np.mean(px_turn1)
+y00 = np.mean(y_turn1)
+py00 = np.mean(py_turn1)
+
+#----------------------------------------------------------------------------------------------------
+
+b.deleteAllParticles()
+b.addParticle(x0,px0,y0,py0,0,0)
+
+x_turn2, px_turn2 = list(), list()
+
+for k in range(turn):
+    teapot_latt.trackBunch(b)
+    x_turn2.append(b.x(0))
+    px_turn2.append(b.px(0))
+
+
+plt.scatter(x_turn2,px_turn2,s=5, color = 'black', label = 'start from 0')
+
+b.deleteAllParticles()
+b.addParticle(x00,px00,y00,py00,0,0)
+
+x_turn3, px_turn3 = list(), list()
+
+for k in range(turn):
+    teapot_latt.trackBunch(b)
+    x_turn3.append(b.x(0))
+    px_turn3.append(b.px(0))
+
+
+plt.scatter(x_turn3,px_turn3,s=5, color = 'grey', label = 'start from 00')
+#----------------------------------------------------------------------------------------------------
+
+plt.scatter(x_turn1,px_turn1,s=5, color = 'red', label = 'distorted orbit')
+plt.scatter([x0],[px0], marker = 'x', color = 'blue', label = 'standart')
+plt.scatter([x00],[px00], marker = 'x', color = 'red', label = 'averaging')
+
+
 plt.grid(True, ls = '--')
 plt.xlabel('x, m')
 plt.ylabel('$p_x$, rad')
-plt.title('One particle in during 1024 consequent turns. Distorted orbit')
-#plt.savefig('phase_space_x_dist1.pdf')
+#plt.legend(loc="lower right")
+plt.legend(loc="best")
+plt.title('One particle in during 1024 consequent turns in ideal and distorted lattices')
+plt.savefig('phase_space.pdf')
 plt.show()
 
 
